b841c27afd0fe27af764708aae0b9756116895ba
Shaza Mohamed705 <mshaza507@gmail.com>
2025-08-20 13:40:28 +0300

    edit code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TransportContract from "./TransportContract/TransportContract";
 import "./App.css";
 
@@ -7,10 +7,7 @@ function App() {
     <BrowserRouter>
       <div className="app">
         <Routes>
-          {/* إعادة توجيه الصفحة الرئيسية إلى الرحلة رقم 22 */}
-          <Route path="/" element={<Navigate to="/trips/22" replace />} />
-
-          {/* باقي الرحلات باستخدام id */}
+          {/* أي id يجي في الرابط يعرض الرحلة */}
           <Route path="/trips/:id" element={<TransportContract />} />
         </Routes>
       </div>
